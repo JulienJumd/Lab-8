08d74882cdd739ebdd6fbc489fcfa6bb2b87724d
JulienJumd <70719249+JulienJumd@users.noreply.github.com>
2020-11-20 15:31:27 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+// Code here for how to set 'public' as the static folder for express
+
+let labRoute = require('./public/js/lab8script')
+
+app.use(express.static('public'))
+
+
+
+
+const PORT = process.env.PORT || 8080
+
+
+// Code here to have the app listen on port 8080....please provide a console.log message
+
+app.listen(PORT, () => {console.log('Server connected at:',PORT);});
+
+
+
+
